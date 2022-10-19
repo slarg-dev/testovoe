4fdd3dce5f79a5e3ff0e33581ba5af00e8e7918c
slarg <62905039+slarg-dev@users.noreply.github.com>
2022-10-19 10:49:42 +0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,138 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 import java.util.Scanner;
 
+public class Calculator {
+    public Calculator() {
+    }
+
+    public static void main(String[] args) {
+        Converter converter = new Converter();
+        String[] actions = new String[]{"+", "-", "/", "*"};
+        String[] regexActions = new String[]{"\\+", "-", "/", "\\*"};
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String exp = scn.nextLine();
+        int actionIndex = -1;
+        int check = 0;
+        for(int i = 0; i < actions.length;i++){
+            if(exp.contains(actions[i])) {
+                actionIndex = i;
+                break;
+            }
+        }
+        check = exp.length() - exp.replace(actions[actionIndex], "").length();
+        if (check > 1) {
+            System.out.println("Error: т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            return;
+        }
+
+            System.out.println(actionIndex);
+            if (actionIndex == -1) {
+                System.out.println("Неверный формат: некорректное выражение");
+            } else {
+                String[] data = exp.split(regexActions[actionIndex]);
+                System.out.println(data[0]);
+                try {
+                    if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+
+                        boolean isRoman = converter.isRoman(data[0]);
+                        int a;
+                        int b;
+                        if (isRoman) {
+                            a = converter.romanToInt(data[0]);
+                            b = converter.romanToInt(data[1]);
+                        } else {
+                            a = Integer.parseInt(data[0]);
+                            b = Integer.parseInt(data[1]);
+                        }
+
+                        int result;
+                        switch (actions[actionIndex]) {
+                            case "+":
+                                result = a + b;
+                                break;
+                            case "-":
+                                result = a - b;
+                                break;
+                            case "*":
+                                result = a * b;
+                                break;
+                            default:
+                                result = a / b;
+                        }
+
+                        if (isRoman) {
+                            System.out.println(converter.intToRoman(result));
+                        } else {
+                            System.out.println(result);
+                        }
+                    } else {
+                        System.out.println(" используются одновременно разные системы счисления");
+                    }
+                } catch (Exception var14) {
+                    System.out.println("Неверный формат: " + var14);
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Converter converter = new Converter();
@@ -10,23 +143,29 @@ public class Calculator {
         String exp = scn.nextLine();
         int actionIndex=-1;
 
-        for (int i = 0; i < exp.length(); i++) {
-            try {
-                if(exp.contains(actions[i])){
-                    //exp.contains(actions[i]);
-                    actionIndex++;
-                }
-            }catch (Exception e){
-             System.out.println("Формат некорректен, калькулятор умеет работать только с двумя числами,  от 1 до 10");
-             return;
-            }
+        int i = 0;
+        for(int s = 0; s < exp.length(); s++) {
+                    int a = exp.length() - exp.replace(actions[i], "").length();
+                    if(exp.contains(actions[i])) {
+                        actionIndex++;
+                    }
+                    if(i >= 3){
+                        i = 0;
+                    }
+                    if(a > 1){
+                        System.out.println("Error: т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                        return;
+                    }
+                    i++;
+
         }
+
         if(actionIndex==-1){
             System.out.println("Неверный формат: некорректное выражение");
             return;
         }
         String[] data = exp.split(regexActions[actionIndex]);
-        try {
+        System.out.println(data[1]);
             if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
                 int a, b;
                 boolean isRoman = converter.isRoman(data[0]);
@@ -62,13 +201,11 @@ public class Calculator {
                     System.out.println(result);
                 }
             } else {
-                System.out.println(" используются одновременно разные системы счисления");
+                System.out.println("Error: используются одновременно разные системы счисления");
             }
-        }catch (Exception e){
-            System.out.println("Неверный формат");
-        }
+
 
 
 
     }
-}
+}*/
--- Converter.java
+++ Converter.java
@@ -1,62 +1,68 @@
+
 import java.util.TreeMap;
 
 public class Converter {
-    TreeMap<Character, Integer> romanKeyMap = new TreeMap<>();
-    TreeMap<Integer, String> arabianKeyMap = new TreeMap<>();
-    public Converter() {
-        romanKeyMap.put('I', 1);
-        romanKeyMap.put('V', 5);
-        romanKeyMap.put('X', 10);
-        romanKeyMap.put('L', 50);
-        romanKeyMap.put('C', 100);
-        romanKeyMap.put('D', 500);
-        romanKeyMap.put('M', 1000);
+    TreeMap<Character, Integer> romanKeyMap = new TreeMap();
+    TreeMap<Integer, String> arabianKeyMap = new TreeMap();
 
-        arabianKeyMap.put(1000, "M");
-        arabianKeyMap.put(900, "CM");
-        arabianKeyMap.put(500, "D");
-        arabianKeyMap.put(400, "CD");
-        arabianKeyMap.put(100, "C");
-        arabianKeyMap.put(90, "XC");
-        arabianKeyMap.put(50, "L");
-        arabianKeyMap.put(40, "XL");
-        arabianKeyMap.put(10, "X");
-        arabianKeyMap.put(9, "IX");
-        arabianKeyMap.put(5, "V");
-        arabianKeyMap.put(4, "IV");
-        arabianKeyMap.put(1, "I");
+    public Converter() {
+        this.romanKeyMap.put('I', 1);
+        this.romanKeyMap.put('V', 5);
+        this.romanKeyMap.put('X', 10);
+        this.romanKeyMap.put('L', 50);
+        this.romanKeyMap.put('C', 100);
+        this.romanKeyMap.put('D', 500);
+        this.romanKeyMap.put('M', 1000);
+        this.arabianKeyMap.put(1000, "M");
+        this.arabianKeyMap.put(900, "CM");
+        this.arabianKeyMap.put(500, "D");
+        this.arabianKeyMap.put(400, "CD");
+        this.arabianKeyMap.put(100, "C");
+        this.arabianKeyMap.put(90, "XC");
+        this.arabianKeyMap.put(50, "L");
+        this.arabianKeyMap.put(40, "XL");
+        this.arabianKeyMap.put(10, "X");
+        this.arabianKeyMap.put(9, "IX");
+        this.arabianKeyMap.put(5, "V");
+        this.arabianKeyMap.put(4, "IV");
+        this.arabianKeyMap.put(1, "I");
     }
-    public boolean isRoman(String number){
-        return romanKeyMap.containsKey(number.charAt(0));
+
+    public boolean isRoman(String number) {
+        return this.romanKeyMap.containsKey(number.charAt(0));
     }
+
     public String intToRoman(int number) {
         String roman = "";
-        int arabianKey;
-        try{do {
-            arabianKey = arabianKeyMap.floorKey(number);
-            roman += arabianKeyMap.get(arabianKey);
-            number -= arabianKey;
-        } while (number != 0); }
-        catch (Exception e) {
+        System.out.println(number);
+        try {
+            do {
+                int arabianKey = (Integer)this.arabianKeyMap.floorKey(number);
+                roman = roman + (String)this.arabianKeyMap.get(arabianKey);
+                number -= arabianKey;
+            } while(number != 0);
+        } catch (Exception var5) {
             System.out.println("в римской системе нет отрицательных чисел");
             roman = "Error";
         }
+
         return roman;
     }
+
     public int romanToInt(String s) {
         int end = s.length() - 1;
         char[] arr = s.toCharArray();
-        int arabian;
-        int result = romanKeyMap.get(arr[end]);
-        for (int i = end - 1; i >= 0; i--) {
-            arabian = romanKeyMap.get(arr[i]);
+        int result = (Integer)this.romanKeyMap.get(arr[end]);
 
-            if (arabian < romanKeyMap.get(arr[i + 1])) {
+        for(int i = end - 1; i >= 0; --i) {
+            int arabian = (Integer)this.romanKeyMap.get(arr[i]);
+            if (arabian < (Integer)this.romanKeyMap.get(arr[i + 1])) {
                 result -= arabian;
             } else {
                 result += arabian;
             }
         }
+
         return result;
     }
 }
