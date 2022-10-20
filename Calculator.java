9d20b82cdde47a1b038bbdf04e8a18b581d7d749
slarg <62905039+slarg-dev@users.noreply.github.com>
2022-10-20 07:35:39 +0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
     public static String calc(String exp) {
         Converter converter = new Converter();
         int actionIndex = -1;
-        int check = 0;
+        int check;
         String StringResult;
         String[] actions = new String[]{"+", "-", "/", "*"};
         String[] regexActions = new String[]{"\\+", "-", "/", "\\*"};
@@ -50,33 +50,23 @@ public class Calculator {
                         b = Integer.parseInt(data[1]);
                     }
 
-                    int result;
-                    switch (actions[actionIndex]) {
-                        case "+":
-                            result = a + b;
-                            break;
-                        case "-":
-                            result = a - b;
-                            break;
-                        case "*":
-                            result = a * b;
-                            break;
-                        default:
-                            result = a / b;
-                    }
+                    int result = switch (actions[actionIndex]) {
+                        case "+" -> a + b;
+                        case "-" -> a - b;
+                        case "*" -> a * b;
+                        default -> a / b;
+                    };
 
                     if (isRoman) {
                         StringResult = converter.intToRoman(result);
-                        return  StringResult;
                     } else {
                         StringResult = Integer.toString(result);
-                        return  StringResult;
                     }
                 } else {
                     StringResult = "Используются одновременно разные системы счисления";
-                    return  StringResult;
 
                 }
+                return  StringResult;
             } catch (Exception var14) {
                 StringResult = "Неверный формат: " + var14;
                 return  StringResult;
@@ -85,134 +75,3 @@ public class Calculator {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import java.util.Scanner;
-public class Calculator {
-    public static void main(String[] args) {
-        Converter converter = new Converter();
-        String[] actions = {"+", "-", "/", "*"};
-        String[] regexActions = {"\\+", "-", "/", "\\*"};
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Введите выражение: ");
-        String exp = scn.nextLine();
-        int actionIndex=-1;
-
-        int i = 0;
-        for(int s = 0; s < exp.length(); s++) {
-                    int a = exp.length() - exp.replace(actions[i], "").length();
-                    if(exp.contains(actions[i])) {
-                        actionIndex++;
-                    }
-                    if(i >= 3){
-                        i = 0;
-                    }
-                    if(a > 1){
-                        System.out.println("Error: т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                        return;
-                    }
-                    i++;
-
-        }
-
-        if(actionIndex==-1){
-            System.out.println("Неверный формат: некорректное выражение");
-            return;
-        }
-        String[] data = exp.split(regexActions[actionIndex]);
-        System.out.println(data[1]);
-            if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
-                int a, b;
-                boolean isRoman = converter.isRoman(data[0]);
-
-
-                if (isRoman) {
-                    a = converter.romanToInt(data[0]);
-                    b = converter.romanToInt(data[1]);
-
-                } else {
-                    a = Integer.parseInt(data[0]);
-                    b = Integer.parseInt(data[1]);
-
-                }
-                int result;
-                switch (actions[actionIndex]) {
-                    case "+":
-                        result = a + b;
-                        break;
-                    case "-":
-                        result = a - b;
-                        break;
-                    case "*":
-                        result = a * b;
-                        break;
-                    default:
-                        result = a / b;
-                        break;
-                }
-                if (isRoman) {
-                    System.out.println(converter.intToRoman(result));
-                } else {
-                    System.out.println(result);
-                }
-            } else {
-                System.out.println("Error: используются одновременно разные системы счисления");
-            }
-
-
-
-
-    }
-}*/
