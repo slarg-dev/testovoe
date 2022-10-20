27904fcabe8e6fa93ae9612e7fdbf5f734f992b6
slarg <62905039+slarg-dev@users.noreply.github.com>
2022-10-20 03:46:06 +0300

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -6,80 +6,87 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public Calculator() {
-    }
-
     public static void main(String[] args) {
-        Converter converter = new Converter();
-        String[] actions = new String[]{"+", "-", "/", "*"};
-        String[] regexActions = new String[]{"\\+", "-", "/", "\\*"};
         Scanner scn = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String exp = scn.nextLine();
+        System.out.println(calc(exp));
+    }
+
+    public static String calc(String exp) {
+        Converter converter = new Converter();
         int actionIndex = -1;
         int check = 0;
-        for(int i = 0; i < actions.length;i++){
-            if(exp.contains(actions[i])) {
+        String StringResult;
+        String[] actions = new String[]{"+", "-", "/", "*"};
+        String[] regexActions = new String[]{"\\+", "-", "/", "\\*"};
+        for (int i = 0; i < actions.length; i++) {
+            if (exp.contains(actions[i])) {
                 actionIndex = i;
                 break;
             }
         }
-
-
-
-            if (actionIndex == -1) {
-                System.out.println("Неверный формат: некорректное выражение");
-            } else {
-                String[] data = exp.split(regexActions[actionIndex]);
-                try {
-                    check = exp.length() - exp.replace(actions[actionIndex], "").length();
-                    if (check > 1) {
-                        System.out.println("Error: т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                        return;
-                    }
-                    if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
-
-                        boolean isRoman = converter.isRoman(data[0]);
-                        int a;
-                        int b;
-                        if (isRoman) {
-                            a = converter.romanToInt(data[0]);
-                            b = converter.romanToInt(data[1]);
-                        } else {
-                            a = Integer.parseInt(data[0]);
-                            b = Integer.parseInt(data[1]);
-                        }
-
-                        int result;
-                        switch (actions[actionIndex]) {
-                            case "+":
-                                result = a + b;
-                                break;
-                            case "-":
-                                result = a - b;
-                                break;
-                            case "*":
-                                result = a * b;
-                                break;
-                            default:
-                                result = a / b;
-                        }
-
-                        if (isRoman) {
-                            System.out.println(converter.intToRoman(result));
-                        } else {
-                            System.out.println(result);
-                        }
-                    } else {
-                        System.out.println(" используются одновременно разные системы счисления");
-                    }
-                } catch (Exception var14) {
-                    System.out.println("Неверный формат: " + var14);
+        if (actionIndex == -1) {
+            StringResult = "Неверный формат: некорректное выражение";
+            return  StringResult;
+        } else {
+            String[] data = exp.split(regexActions[actionIndex]);
+            try {
+                check = exp.length() - exp.replace(actions[actionIndex], "").length();
+                if (check > 1) {
+                    StringResult = "Должно быть одно выражение";
+                    return  StringResult;
                 }
+                if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
 
+                    boolean isRoman = converter.isRoman(data[0]);
+                    int a;
+                    int b;
+                    if (isRoman) {
+                        a = converter.romanToInt(data[0]);
+                        b = converter.romanToInt(data[1]);
+                    } else {
+                        a = Integer.parseInt(data[0]);
+                        b = Integer.parseInt(data[1]);
+                    }
+
+                    int result;
+                    switch (actions[actionIndex]) {
+                        case "+":
+                            result = a + b;
+                            break;
+                        case "-":
+                            result = a - b;
+                            break;
+                        case "*":
+                            result = a * b;
+                            break;
+                        default:
+                            result = a / b;
+                    }
+
+                    if (isRoman) {
+                        StringResult = converter.intToRoman(result);
+                        return  StringResult;
+                    } else {
+                        StringResult = Integer.toString(result);
+                        return  StringResult;
+                    }
+                } else {
+                    StringResult = "Используются одновременно разные системы счисления";
+                    return  StringResult;
+
+                }
+            } catch (Exception var14) {
+                StringResult = "Неверный формат: " + var14;
+                return  StringResult;
             }
         }
     }
+}
+
+
+
 
 
 
